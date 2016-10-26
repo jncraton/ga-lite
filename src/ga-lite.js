b5ba9a5e668924f729c3fa9e8a686aecf86ad40e
Jon Craton <jncraton@gmail.com>
2016-10-26 18:40:49 -0400

    Remove load event wrap

--- src/ga-lite.js
+++ src/ga-lite.js
@@ -1,82 +1,80 @@
 (function(window, localStorage, navigator, screen, document, encodeURIComponent) {
-    window.addEventListener('load', function() {
-        var pageLoadedTimestamp = new Date().getTime();
+    var pageLoadedTimestamp = new Date().getTime();
 
-        var urlBase = (
-            '//google-analytics.com/collect?' +
-            'cid=' + (localStorage.uid = localStorage.uid || Math.random() + '.' + Math.random()) +
-            '&v=1' +
-            '&tid={your_UA}' +
-            '&dl=' + encodeURIComponent(location)
-        );
+    var urlBase = (
+        '//google-analytics.com/collect?' +
+        'cid=' + (localStorage.uid = localStorage.uid || Math.random() + '.' + Math.random()) +
+        '&v=1' +
+        '&tid={your_UA}' +
+        '&dl=' + encodeURIComponent(location)
+    );
 
-        var getOptionalStr = function(values) {
-            var str = '';
-            for (var i in values) {
-                if (values[i] === undefined) {
-                    return false;
-                }
-                str += encodeURIComponent(values[i]);
+    var getOptionalStr = function(values) {
+        var str = '';
+        for (var i in values) {
+            if (values[i] === undefined) {
+                return false;
             }
-            return str;
-        };
-
-        var optional = {
-            'dt': [document.title],
-            'sd': [screen.colorDepth, '-bit'],
-            'sr': [screen.availHeight, 'x', screen.availWidth],
-            'vp': [innerWidth, 'x', innerHeight],
-            'dr': [document.referrer]
-        };
-        for (var key in optional) {
-            var value = key + '=' + getOptionalStr(optional[key]);
-            if (value) {
-                urlBase += '&' + value;
-            }
+            str += encodeURIComponent(values[i]);
         }
+        return str;
+    };
 
-        var sendTo = function(url) {
-            if (navigator.sendBeacon) {
-                navigator.sendBeacon(url);
-            } else {
-                var i = new Image();
-                i.src = url;
-            }
+    var optional = {
+        'dt': [document.title],
+        'sd': [screen.colorDepth, '-bit'],
+        'sr': [screen.availHeight, 'x', screen.availWidth],
+        'vp': [innerWidth, 'x', innerHeight],
+        'dr': [document.referrer]
+    };
+    for (var key in optional) {
+        var value = key + '=' + getOptionalStr(optional[key]);
+        if (value) {
+            urlBase += '&' + value;
+        }
+    }
+
+    var sendTo = function(url) {
+        if (navigator.sendBeacon) {
+            navigator.sendBeacon(url);
+        } else {
+            var i = new Image();
+            i.src = url;
+        }
+    };
+
+    var eventBuilder = function(event, params) {
+        var paramsStr = '';
+        for (var key in params) {
+            paramsStr = '&' + key + '=' + encodeURIComponent(params[key]);
+        }
+        return function() {
+            sendTo(
+                urlBase +
+                paramsStr +
+                '&t=' + encodeURIComponent(event) +
+                '&z=' + new Date().getTime()
+            );
         };
+    };
 
-        var eventBuilder = function(event, params) {
-            var paramsStr = '';
-            for (var key in params) {
-                paramsStr = '&' + key + '=' + encodeURIComponent(params[key]);
+    // Deplay the page load event by 100ms
+    setTimeout(eventBuilder('pageview'), 100);
+
+    /**
+     * Note:
+     * unload event does not fire on:
+     * - Android chrome on tab closing
+     */
+    window.addEventListener(
+        'unload',
+        eventBuilder(
+            'timing',
+            {
+                'utc': 'JS Dependencies',
+                'utv': 'unload',
+                'utt': (new Date().getTime() - pageLoadedTimestamp)
             }
-            return function() {
-                sendTo(
-                    urlBase +
-                    paramsStr +
-                    '&t=' + encodeURIComponent(event) +
-                    '&z=' + new Date().getTime()
-                );
-            };
-        };
-
-        // Deplay the page load event by 100ms
-        setTimeout(eventBuilder('pageview'), 100);
-
-        /**
-         * Note:
-         * unload event does not fire on:
-         * - Android chrome on tab closing
-         */
-        window.addEventListener(
-            'unload',
-            eventBuilder(
-                'timing',
-                {
-                    'utc': 'JS Dependencies',
-                    'utv': 'unload',
-                    'utt': (new Date().getTime() - pageLoadedTimestamp)
-                }
-            )
-        );
-    });
+        )
+    );
 })(window, localStorage, navigator, screen, document, encodeURIComponent);
