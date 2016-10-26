afe15d98ee70848ab877ac57848ec7c1fc994e17
Jon Craton <jncraton@gmail.com>
2016-10-26 19:10:45 -0400

    Removed optional params

--- ga-micro.js
+++ ga-micro.js
@@ -1,4 +1,4 @@
-(function(window, localStorage, navigator, screen, document, encodeURIComponent) {
+(function(window, localStorage, navigator, document, encodeURIComponent) {
     var pageLoadedTimestamp = new Date().getTime();
 
     var urlBase = (
@@ -6,60 +6,28 @@
         'cid=' + (localStorage.uid = localStorage.uid || Math.random() + '.' + Math.random()) +
         '&v=1' +
         '&tid={your_UA}' +
-        '&dl=' + encodeURIComponent(location)
+        '&dl=' + encodeURIComponent(location) +
+        '&dt=' + encodeURIComponent(document.title) +
+        '&dr=' + encodeURIComponent(document.referrer)
     );
 
-    var getOptionalStr = function(values) {
-        var str = '';
-        for (var i in values) {
-            if (values[i] === undefined) {
-                return false;
-            }
-            str += encodeURIComponent(values[i]);
-        }
-        return str;
-    };
-
-    var optional = {
-        'dt': [document.title],
-        'sd': [screen.colorDepth, '-bit'],
-        'sr': [screen.availHeight, 'x', screen.availWidth],
-        'vp': [innerWidth, 'x', innerHeight],
-        'dr': [document.referrer]
-    };
-    for (var key in optional) {
-        var value = key + '=' + getOptionalStr(optional[key]);
-        if (value) {
-            urlBase += '&' + value;
-        }
-    }
-
-    var sendTo = function(url) {
-        if (navigator.sendBeacon) {
-            navigator.sendBeacon(url);
-        } else {
-            var i = new Image();
-            i.src = url;
-        }
-    };
-
     var eventBuilder = function(event, params) {
-        var paramsStr = '';
-        for (var key in params) {
-            paramsStr = '&' + key + '=' + encodeURIComponent(params[key]);
-        }
         return function() {
-            sendTo(
-                urlBase +
-                paramsStr +
+            url = urlBase + params +
                 '&t=' + encodeURIComponent(event) +
                 '&z=' + new Date().getTime()
-            );
+
+            if (navigator.sendBeacon) {
+                navigator.sendBeacon(url);
+            } else {
+                var i = new Image();
+                i.src = url;
+            }
         };
     };
 
     // Deplay the page load event by 100ms
-    setTimeout(eventBuilder('pageview'), 100);
+    setTimeout(eventBuilder('pageview', ''), 100);
 
     /**
      * Note:
@@ -70,11 +38,7 @@
         'unload',
         eventBuilder(
             'timing',
-            {
-                'utc': 'JS Dependencies',
-                'utv': 'unload',
-                'utt': (new Date().getTime() - pageLoadedTimestamp)
-            }
+            '&utc=JS Dependencies&utv=unload&utt='+ (new Date().getTime() - pageLoadedTimestamp)
         )
     );
-})(window, localStorage, navigator, screen, document, encodeURIComponent);
+})(window, localStorage, navigator, document, encodeURIComponent);
