5861cf338556ef6cc6010f115f741edc02ef2b21
Jon Craton <jncraton@gmail.com>
2016-10-26 20:46:07 -0400

    Remove unload event handling

--- ga-micro.js
+++ ga-micro.js
@@ -1,44 +1,18 @@
-(function(window, localStorage, navigator, document, encodeURIComponent, Math_random) {
-    var pageLoadedTimestamp;
-        
-    var callHome = function() {
-        if (pageLoadedTimestamp) {
-          var params =  't=timing&utc=JS Dependencies&utv=unload&utt='+ (new Date().getTime() - pageLoadedTimestamp)
-        } else {
-          var params = 'pageview'
-        }
-        
-        var url = 
-          '//google-analytics.com/collect?' +
-          '&tid={your_UA}' +
-          '&v=1&' +
-          '&t=' + params + 
-          '&cid=' + (localStorage.uid = localStorage.uid || Math_random() + '.' + Math_random()) +
-          '&dl=' + encodeURIComponent(location) +
-          '&dt=' + encodeURIComponent(document.title) +
-          '&dr=' + encodeURIComponent(document.referrer) +
-          '&z=' + Math_random()
+(function(localStorage, navigator, document, encodeURIComponent, Math_random) {
+  var url = 
+    '//google-analytics.com/collect?' +
+    '&tid={your_UA}' +
+    '&v=1&t=pageview' +
+    '&cid=' + (localStorage.uid = localStorage.uid || Math_random() + '.' + Math_random()) +
+    '&dl=' + encodeURIComponent(location) +
+    '&dt=' + encodeURIComponent(document.title) +
+    '&dr=' + encodeURIComponent(document.referrer) +
+    '&z=' + Math_random()
 
-        pageLoadedTimestamp = new Date().getTime();
-
-        if (navigator && navigator.sendBeacon) {
-            navigator.sendBeacon(url);
-        } else {
-            var i = new Image();
-            i.src = url;
-        }
-
-    };
-
-    /**
-     * Note:
-     * unload event does not fire on:
-     * - Android chrome on tab closing
-     */
-    window.addEventListener(
-        'unload',
-        callHome
-    );
-
-    callHome();
-})(window, localStorage, navigator, document, encodeURIComponent, Math.random);
+  if (navigator && navigator.sendBeacon) {
+      navigator.sendBeacon(url);
+  } else {
+      var i = new Image();
+      i.src = url;
+  }
+})(localStorage, navigator, document, encodeURIComponent, Math.random);
