a638a74f87461f24143d667086b357d48a09e875
Jon Craton <jncraton@gmail.com>
2016-10-26 20:02:23 -0400

    Update state after first call instead of using function param

--- ga-micro.js
+++ ga-micro.js
@@ -1,30 +1,30 @@
 (function(window, localStorage, navigator, document, encodeURIComponent, Math_random) {
     var pageLoadedTimestamp = new Date().getTime();
+    var params = 't=pageview';
+    var callHome = function() {
+        var url = 
+          '//google-analytics.com/collect?' +
+          '&tid={your_UA}' +
+          '&v=1&' +
+          params + 
+          '&cid=' + (localStorage.uid = localStorage.uid || Math_random() + '.' + Math_random()) +
+          '&dl=' + encodeURIComponent(location) +
+          '&dt=' + encodeURIComponent(document.title) +
+          '&dr=' + encodeURIComponent(document.referrer) +
+          '&z=' + Math_random()
 
-    var eventBuilder = function(params) {
-        return function() {
-            var url = 
-              '//google-analytics.com/collect?' +
-              '&tid={your_UA}' +
-              '&v=1' +
-              '&cid=' + (localStorage.uid = localStorage.uid || Math_random() + '.' + Math_random()) +
-              '&dl=' + encodeURIComponent(location) +
-              '&dt=' + encodeURIComponent(document.title) +
-              '&dr=' + encodeURIComponent(document.referrer) +
-              params +
-              '&z=' + Math_random()
+        if (navigator && navigator.sendBeacon) {
+            navigator.sendBeacon(url);
+        } else {
+            var i = new Image();
+            i.src = url;
+        }
 
-            if (navigator && navigator.sendBeacon) {
-                navigator.sendBeacon(url);
-            } else {
-                var i = new Image();
-                i.src = url;
-            }
-        };
+        params =  't=timing&utc=JS Dependencies&utv=unload&utt='+ (new Date().getTime() - pageLoadedTimestamp)
     };
 
     // Deplay the page load event by 100ms
-    setTimeout(eventBuilder('&t=pageview'), 100);
+    setTimeout(callHome, 100);
 
     /**
      * Note:
@@ -33,8 +33,6 @@
      */
     window.addEventListener(
         'unload',
-        eventBuilder(
-            '&t=timing&utc=JS Dependencies&utv=unload&utt='+ (new Date().getTime() - pageLoadedTimestamp)
-        )
+        callHome
     );
 })(window, localStorage, navigator, document, encodeURIComponent, Math.random);
